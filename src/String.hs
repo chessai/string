25bcae6ec70aa57bc5504c877ccb96543dc11370
chessai <chessai1996@gmail.com>
2020-12-12 14:59:30 -0800

    rewrite (it works)

--- src/String.hs
+++ src/String.hs
@@ -1,10 +1,13 @@
 {-# language
     BangPatterns
+  , DerivingStrategies
+  , GeneralizedNewtypeDeriving
   , MagicHash
   , MultiWayIf
   , OverloadedStrings
   , RankNTypes
   , ScopedTypeVariables
+  , StandaloneDeriving
   , TypeFamilies
   , UnboxedTuples
   , ViewPatterns
@@ -12,17 +15,20 @@
 
 module String where
 
-import Data.Bits ((.&.), (.|.), unsafeShiftL)
+import Data.Bits ((.&.))
+import Numeric (showHex)
+import Data.Bytes.Types (Bytes(..))
 import Data.Primitive.ByteArray
-import GHC.Word (Word8(..), Word16(..), Word32(..))
-import GHC.Exts hiding (build, toList)
-
-import qualified GHC.Exts as Exts
-import qualified Data.List as List
-
-import Prelude hiding (String, length, Foldable(..))
-
 import Debug.Trace
+import GHC.Exts hiding (build, toList)
+import GHC.Word (Word8(..), Word16(..), Word32(..))
+import Prelude hiding (String, length, Foldable(..))
+import qualified Data.Bytes as Bytes
+import qualified Data.List as List
+import qualified GHC.Exts as Exts
+import qualified Data.Bytes.Chunks as Chunks
+import qualified Data.Bytes.Builder as Builder
+import qualified Prelude
 
 {-
 -- querying
@@ -42,185 +48,124 @@ fromUtf16Lossy :: PrimArray Word8 -> String
 fromUtf32Lossy :: PrimArray Word8 -> String
 -}
 
-test :: IO ()
-test = do
-  --print $ byteArrayFromList ['1', '2', '3', '4']
-  print $ String.fromList "1234"
-
 -----------
 -- Types --
 -----------
 
-data String = String
-  ByteArray# -- buffer
-  Int#       -- offset
-  Int#       -- length
-
---data MutableString s = MutableString
---  (MutableByteArray# s) -- buffer
---  Int#                  -- offset
---  Int#                  -- length
+newtype String = String Bytes
 
 ---------------
 -- Instances --
 ---------------
 
-instance Eq String where
-  String buf0 off0 len0 == String buf1 off1 len1
-    | neq len0 len1 = False
-    | neq off0 off1 = False
-    | sameByteArray# buf0 buf1 = True
-    | otherwise =
-        case compareByteArrays# buf0 off0 buf1 off1 len0 of
-          0# -> True
-          _  -> False
-  {-# inline (==) #-}
-
-instance Ord String where
-  compare = todo
-
-instance Semigroup String where
-  (<>) = append
-  {-# inline (<>) #-}
-
-instance Monoid String where
-  mempty = empty
-  {-# inline mempty #-}
+deriving newtype instance Eq String
+deriving newtype instance Ord String
+deriving newtype instance Semigroup String
+deriving newtype instance Monoid String
 
 instance IsString String where
   fromString = String.fromList
 
 instance IsList String where
   type Item String = Char
-  fromListN = todo
-  fromList  = todo
-  toList    = toList
+  fromListN = todo -- String.fromListN
+  fromList  = String.fromList
+  toList    = String.toList
 
 instance Show String where
-  show s = "\"" ++ toList s ++ "\""
-
-debugString :: String -> [Char]
-debugString (String _ o l) = "String { off = "
-  ++ show (I# o)
-  ++ ", len = "
-  ++ show (I# l)
-  ++ " }"
+  show s = "\"" ++ String.toList s ++ "\""
 
 ----------------
 -- Public API --
 ----------------
 
 append :: String -> String -> String
-append (String _ _ 0#) b = b
-append a (String _ _ 0#) = a
-append (String buf0 off0 len0) (String buf1 off1 len1) =
-  let len = len0 +# len1
-      buf = runByteArrayST $ \s0 ->
-        case newByteArray# len s0 of
-          (# s1, mb #) -> case copyByteArray# buf0 off0 mb 0# len0 s1 of
-            s2 -> case copyByteArray# buf1 off1 mb len1 len1 s2 of
-              s3 -> case unsafeFreezeByteArray# mb s3 of
-                (# s4, b #) -> (# s4, b #)
-  in String buf 0# len
-{-# noinline append #-}
+append (String x) (String y) = String (x <> y)
+{-# inline append #-}
 
 empty :: String
-empty = String (case mempty of { ByteArray b -> b }) 0# 0#
+empty = String Bytes.empty
 {-# inline empty #-}
 
 toList :: String -> [Char]
-toList (String buf _ _) = show (ByteArray buf) -- Exts.build (\c n -> foldr c n xs)
+toList xs = Exts.build (\c n -> foldr c n xs)
 {-# inline toList #-}
 
 fromList :: [Char] -> String
-fromList xs =
-  let listLen = case List.length xs of { I# l -> l }
-      buf = runByteArrayST $ \s0 -> case newByteArray# listLen s0 of {
-        (# s1, arr #) ->
-          let go _ acc [] s = (# s, acc #)
-              go i acc (C# c : cs) s = case writeChar arr i c s of {
-                (# s', bytesWritten #) -> go (i +# bytesWritten +# 1#) (acc +# bytesWritten) cs s'
-              }
-          in case go 0# 0# xs s1 of {
-               (# s2, size #) -> case shrinkMutableByteArray# arr size s2 of {
-                 s3 -> unsafeFreezeByteArray# arr s3
-               }
-             }
-      }
-      len = sizeofByteArray# buf
-  in String buf 0# len
-{-# inline fromList #-}
+fromList xs = String
+  $ Chunks.concat
+  $ Builder.run 4080
+  $ Prelude.foldMap Builder.char xs
 
 foldr :: (Char -> b -> b) -> b -> String -> b
-foldr f z0 = \s@(String _ off len) ->
-  let end = off +# len
-      go ix
-        | isTrue# (end ># ix) = case nextChar s ix of
-            Iter ix' c -> f (C# c) (go ix')
-        | otherwise = z0
-  in go 0#
+foldr f z0 = \s@(String (Bytes _ off0 len0)) ->
+  let go !off !len
+        | len == 0 = z0
+        | otherwise = case traceShowId (nextChar s off) of
+            Next bytesConsumed c -> f c (go (off + bytesConsumed) (len - bytesConsumed))
+  in go off0 len0
 {-# inline foldr #-}
 
 foldl' :: (b -> Char -> b) -> b -> String -> b
-foldl' f z0 = \s@(String _ off len) ->
-  let end = off +# len
-      go ix !acc
-        | isTrue# (ix <# end) = case nextChar s ix of
-            Iter ix' c -> go ix' (f acc (C# c))
-        | otherwise = acc
-  in go 0# z0
+foldl' f z0 = \s@(String (Bytes _ off0 len0)) ->
+  let go !acc !off !len
+        | len == 0 = acc
+        | otherwise = case nextChar s off of
+            Next bytesConsumed c -> go (f acc c) (off + bytesConsumed) (len - bytesConsumed)
+  in go z0 off0 len0
 {-# inline foldl' #-}
 
-data Iter = Iter
-  Int#  -- next byte to index
-  Char# -- char at the previous byte index
+data Iter = Next Int Char
+  deriving (Show)
 
-instance Show Iter where
-  show (Iter i# c#) = "Iter " ++ show (I# i#) ++ " " ++ show (C# c#)
+nextChar :: String -> Int -> Iter
+nextChar (String bytes) i =
+  let header = Bytes.unsafeIndex bytes i
+  in if | iub8  header -> Next 1 (chr8 header)
+        | iub16 header -> Next 2 (chr16 header (Bytes.unsafeIndex bytes (i + 1)))
+        | iub24 header -> Next 3 (chr24 header (Bytes.unsafeIndex bytes (i + 1)) (Bytes.unsafeIndex bytes (i + 2)))
+        | iub32 header -> Next 4 (chr32 header (Bytes.unsafeIndex bytes (i + 1)) (Bytes.unsafeIndex bytes (i + 2)) (Bytes.unsafeIndex bytes (i + 3)))
+        | otherwise    -> error $ "Invalid UTF-8 code point: " ++ showHex header ""
 
--- iterate through the string.
-nextChar :: String -> Int# -> Iter
-nextChar (String buf off _) i =
-  case indexWord8Array# buf (off +# i) of
-    w0 -> let w = W8# w0
-          in if | iub8  w -> let c = chr8 w0
-                             in Iter (i +# 1#) c
-                | iub16 w -> let w1 = indexWord8Array# buf (off +# i +# 1#)
-                                 c  = chr16 w0 w1
-                             in Iter (i +# 2#) c
-                | iub24 w -> let w1 = indexWord8Array# buf (off +# i +# 1#)
-                                 w2 = indexWord8Array# buf (off +# i +# 2#)
-                                 c  = chr24 w0 w1 w2
-                             in Iter (i +# 3#) c
-                | iub32 w -> let w1 = indexWord8Array# buf (off +# i +# 1#)
-                                 w2 = indexWord8Array# buf (off +# i +# 2#)
-                                 w3 = indexWord8Array# buf (off +# i +# 3#)
-                                 c  = chr32 w0 w1 w2 w3
-                             in Iter (i +# 4#) c
-                | otherwise -> error $ "invalid UTF-8 header byte [" ++ show (I# i) ++ "]: " ++ show w
-{-# noinline nextChar #-}
+chr8 :: Word8 -> Char
+chr8 (W8# x) = C# (chr# (word2Int# x))
+{-# inline chr8 #-}
 
--------------
--- Helpers --
--------------
+chr16 :: Word8 -> Word8 -> Char
+chr16 (W8# x1#) (W8# x2#) = C# (chr# (z1# +# z2#))
+  where
+    !y1# = word2Int# x1#
+    !y2# = word2Int# x2#
+    !z1# = uncheckedIShiftL# (y1# -# 0xC0#) 6#
+    !z2# = y2# -# 0x80#
+{-# inline chr16 #-}
 
-sameByteArray# :: ByteArray# -> ByteArray# -> Bool
-sameByteArray# b0# b1# =
-  case sameMutableByteArray# (unsafeCoerce# b0#) (unsafeCoerce# b1#) of
-    r -> isTrue# r
-{-# inline sameByteArray# #-}
+chr24 :: Word8 -> Word8 -> Word8 -> Char
+chr24 (W8# x1#) (W8# x2#) (W8# x3#) = C# (chr# (z1# +# z2# +# z3#))
+  where
+    !y1# = word2Int# x1#
+    !y2# = word2Int# x2#
+    !y3# = word2Int# x3#
+    !z1# = uncheckedIShiftL# (y1# -# 0xE0#) 12#
+    !z2# = uncheckedIShiftL# (y2# -# 0x80#) 6#
+    !z3# = y3# -# 0x80#
+{-# inline chr24 #-}
 
-neq :: Int# -> Int# -> Bool
-neq x y = case x ==# y of { 0# -> True; _ -> False; }
-{-# inline neq #-}
-
-runByteArrayST :: (forall s. State# s -> (# State# s, ByteArray# #)) -> ByteArray#
-runByteArrayST f = runRW# (\s0 -> case f s0 of { (# _, r #) -> r })
-{-# noinline runByteArrayST #-}
+chr32 :: Word8 -> Word8 -> Word8 -> Word8 -> Char
+chr32 (W8# x1#) (W8# x2#) (W8# x3#) (W8# x4#) = C# (chr# (z1# +# z2# +# z3# +# z4#))
+  where
+    !y1# = word2Int# x1#
+    !y2# = word2Int# x2#
+    !y3# = word2Int# x3#
+    !y4# = word2Int# x4#
+    !z1# = uncheckedIShiftL# (y1# -# 0xF0#) 18#
+    !z2# = uncheckedIShiftL# (y2# -# 0x80#) 12#
+    !z3# = uncheckedIShiftL# (y3# -# 0x80#) 6#
+    !z4# = y4# -# 0x80#
+{-# inline chr32 #-}
 
 todo :: a
 todo = error "TODO"
-{-# noinline todo #-}
 
 -- Check if the header byte indicates a
 -- first-plane byte sequence.
@@ -295,53 +240,3 @@ iub32 w = w .&. 0xF8 == 0xF0
 iubO :: Word8 -> Bool
 iubO w = w .&. 0xC0 == 0x80
 {-# inline iubO #-}
-
-chr8 :: Word# -> Char#
-chr8 w = chr# (word2Int# w)
-
-chr16 :: Word# -> Word# -> Char#
-chr16 w0# w1# =
-  let w0 = w8_2_w16 (W8# w0#)
-      w1 = w8_2_w16 (W8# w1#)
-  in case unsafeShiftL w0 8 .|. w1 of
-       W16# w -> chr# (word2Int# w)
-
-chr24 :: Word# -> Word# -> Word# -> Char#
-chr24 w0# w1# w2# =
-  let w0 = w8_2_w32 (W8# w0#)
-      w1 = w8_2_w32 (W8# w1#)
-      w2 = w8_2_w32 (W8# w2#)
-  in case unsafeShiftL w0 16 .|. unsafeShiftL w1 8 .|. w2 of
-       W32# w -> chr# (word2Int# w)
-
-chr32 :: Word# -> Word# -> Word# -> Word# -> Char#
-chr32 w0# w1# w2# w3# =
-  let w0 = w8_2_w32 (W8# w0#)
-      w1 = w8_2_w32 (W8# w1#)
-      w2 = w8_2_w32 (W8# w2#)
-      w3 = w8_2_w32 (W8# w3#)
-  in case unsafeShiftL w0 24 .|. unsafeShiftL w1 16 .|. unsafeShiftL w2 8 .|. w3 of
-       W32# w -> chr# (word2Int# w)
-
-w8_2_w16 :: Word8 -> Word16
-w8_2_w16 = fromIntegral
-
-w8_2_w32 :: Word8 -> Word32
-w8_2_w32 = fromIntegral
-
-writeChar :: MutableByteArray# s -> Int# -> Char# -> State# s -> (# State# s, Int# #)
-writeChar m i (ord# -> c) = \s0 ->
-  case c <=# 0x7f# of -- plane 1 check
-    0# -> case c <=# 0x7ff# of -- plane 2 check
-      0# -> case c <=# 0xffff# of -- plane 3 check
-        0# -> case writeWord32Array# m i (int2Word# c) s0 of { s1 -> (# s1, 4# #); }
-        -- for plane 3, there is no writeWord24Array#, so we must write the
-        -- bytes in multiple instructions
-        _  -> case int2Word# c of {
-          w -> case writeWord8Array# m i (plusWord# 0xe0## (uncheckedShiftRL64# w 12#)) s0 of {
-            s1 -> case writeWord8Array# m (i +# 1#) (plusWord# 0x80## (and# (uncheckedShiftRL64# w 6#) 0x3f##)) s1 of {
-              s2 -> case writeWord8Array# m (i +# 2#) (and# (plusWord# 0x80## w) 0x3f##) s2 of {
-                s3 -> (# s3, 3# #)
-        }}}}
-      _  -> case writeWord16Array# m i (int2Word# c) s0 of { s1 -> (# s1, 2# #); }
-    _  -> case writeWord8Array# m i (int2Word# c) s0 of { s1 -> (# s1, 1# #); }
